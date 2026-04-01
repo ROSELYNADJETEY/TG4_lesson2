825a3aaa6a669411570b91877525f7bd47182090
ROSELYNADJETEY <roselynmingle386@gamil.com>
2026-04-01 07:12:02 -0700

    addition

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// console.log("Hello World!");
+
+// let x = 15;
+// console.log(x);//15
+
+// x = 10;
+// console.log(x);//10
+
+// let answer = prompt("What is your age now?");
+// console.log(answer);
+
+// answer =prompt("What is your age in five years?");
+// console.log(answer);
+
+let num1 = 7;
+let num2 = 13;
+
+console.log(num1 + num2); //20
+console.log(num2 - num1); //6
+console.log(num1 * num2); //91
+console.log(num1 / num2); //0.54
+console.log(num2 % num1); //6
+
+let number = 6; // assigned and initilaize the variable
+if(number%2 == 0){//compering the values on the left to the  right in the condition.
+alert (`${number} is even.`);// if the condition is met,it  alert that.
+ alert( number + "is even")//if the condition is met ,it alert this.
+console.log( number + "is even" )//
+};
+if("")else{}
